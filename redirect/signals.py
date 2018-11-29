2202874abf01b5cd3b225ffe66d4d21884312d78
Ryan George <ryan.kris.george@gmail.com>
2018-11-29 10:55:14 -0700

    fix cache delete

--- redirect/signals.py
+++ redirect/signals.py
@@ -1,5 +1,5 @@
 from redirect.models import Redirect
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
 
