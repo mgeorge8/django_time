55decabe0de69e0cf665e1b592401610917ce058
Ryan George <ryan.kris.george@gmail.com>
2018-11-29 10:52:36 -0700

    change cache delete

--- redirect/signals.py
+++ redirect/signals.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.core.cache import cache
 
 @receiver(post_save, sender=Redirect)
+@receiver(post_delete, sender=Redirect)
 def reset_cache(sender, instance, **kwargs):
     path = instance.old_path
     cache.delete(path)
