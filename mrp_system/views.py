5c5f3f413f52bcca209ede650e62b716080096e1
Ryan George <ryan.kris.george@gmail.com>
2019-01-25 14:04:10 -0700

    add certs

--- mrp_system/views.py
+++ mrp_system/views.py
@@ -706,7 +706,7 @@ def enter_digi_part(request):
                     #try:
                     datasheet_name = urlparse(datasheet_url).path.split('/')[-1]
                     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0'}
-                    response = requests.get(datasheet_url, headers=headers, timeout=5, verify=False)
+                    response = requests.get(datasheet_url, headers=headers, timeout=5)
                     if response.status_code == 200:
                         new_part.datasheet.save(datasheet_name, ContentFile(response.content), save=True)
                     #except (requests.exceptions.SSLError):
